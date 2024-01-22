e132d0331b8209fbc918a6d72c3f151ae9b8a47e
beyzakoser <b.koser@hotmail.com>
2024-01-22 05:45:05 +0300

    ui update

--- flutterfirebase/lib/screens/chat.dart
+++ flutterfirebase/lib/screens/chat.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfirebase/model/message.dart';
+import 'package:intl/intl.dart';
 
 final firebaseAuthInstance = FirebaseAuth.instance;
 final firebaseFireStore = FirebaseFirestore.instance;
@@ -25,65 +26,75 @@ class _ChatState extends State<Chat> {
       appBar: AppBar(
         title: Text(widget.receiverEmail),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: StreamBuilder(
-              stream: getMessages(
-                  widget.receiverUid, firebaseAuthInstance.currentUser!.uid),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return const Text("Bir hata oluştu");
-                }
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-                return ListView(
-                  children: snapshot.data!.docs
-                      .map<Widget>((e) => addMessage(e))
-                      .toList(),
-                );
-              },
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder(
+                stream: getMessages(
+                    widget.receiverUid, firebaseAuthInstance.currentUser!.uid),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return const Text("Bir hata oluştu");
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                  return ListView(
+                    children: snapshot.data!.docs
+                        .map<Widget>((e) => addMessage(e))
+                        .toList(),
+                  );
+                },
+              ),
             ),
-          ),
-          TextField(
-            controller: _controller,
-            autocorrect: false,
-            decoration: const InputDecoration(
-              labelText: 'Mesaj Giriniz..',
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      labelText: 'Mesaj Giriniz..',
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  child: const Icon(
+                    Icons.send,
+                  ),
+                  onTap: () async {
+                    if (_controller.text.isNotEmpty) {
+                      final String currentUserId =
+                          firebaseAuthInstance.currentUser!.uid;
+                      final String currentUserEmail =
+                          firebaseAuthInstance.currentUser!.email.toString();
+                      final DateTime date = DateTime.now();
+                      Message newMessage = Message(
+                          senderId: currentUserId,
+                          senderEmail: currentUserEmail,
+                          receiverId: widget.receiverUid,
+                          message: _controller.text,
+                          date: date);
+                      List<String> id = [currentUserId, widget.receiverUid];
+                      id.sort();
+                      String chatId = id.join("_");
+                      await firebaseFireStore
+                          .collection("chats")
+                          .doc(chatId)
+                          .collection("messages")
+                          .add(newMessage.toMap());
+                    }
+                    _controller.clear();
+                  },
+                ),
+              ],
             ),
-          ),
-          GestureDetector(
-            child: const Icon(
-              Icons.send,
-            ),
-            onTap: () async {
-              if (_controller.text.isNotEmpty) {
-                final String currentUserId =
-                    firebaseAuthInstance.currentUser!.uid;
-                final String currentUserEmail =
-                    firebaseAuthInstance.currentUser!.email.toString();
-                final DateTime date = DateTime.now();
-                Message newMessage = Message(
-                    senderId: currentUserId,
-                    senderEmail: currentUserEmail,
-                    receiverId: widget.receiverUid,
-                    message: _controller.text,
-                    date: date);
-                List<String> id = [currentUserId, widget.receiverUid];
-                id.sort();
-                String chatId = id.join("_");
-                await firebaseFireStore
-                    .collection("chats")
-                    .doc(chatId)
-                    .collection("messages")
-                    .add(newMessage.toMap());
-              }
-              _controller.clear();
-            },
-          ),
-          const SizedBox(height: 50),
-        ],
+            const SizedBox(height: 50),
+          ],
+        ),
       ),
     );
   }
@@ -93,10 +104,41 @@ class _ChatState extends State<Chat> {
     var alignment = (data["senderId"] == firebaseAuthInstance.currentUser!.uid)
         ? Alignment.centerRight
         : Alignment.centerLeft;
+    var borderRadius =
+        (data["senderId"] == firebaseAuthInstance.currentUser!.uid)
+            ? const BorderRadius.only(
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                topLeft: Radius.circular(30),
+              )
+            : const BorderRadius.only(
+                topRight: Radius.circular(30),
+                bottomLeft: Radius.circular(30),
+                bottomRight: Radius.circular(30),
+              );
+    var color = (data["senderId"] == firebaseAuthInstance.currentUser!.uid)
+        ? Colors.brown[200]
+        : Colors.brown[100];
+    Timestamp timestamp = data["date"];
+    DateTime dateTime = timestamp.toDate();
+    String formattedDate = DateFormat('EEEE HH:mm').format(dateTime);
+
     return Container(
       alignment: alignment,
-      child:
-          Column(children: [Text(data["senderEmail"]), Text(data["message"])]),
+      child: Column(children: [
+        Container(
+          padding: const EdgeInsets.all(12),
+          decoration: BoxDecoration(color: color, borderRadius: borderRadius),
+          child: Text(
+            data["message"],
+            style: const TextStyle(fontWeight: FontWeight.w600),
+          ),
+        ),
+        Text(
+          formattedDate,
+          style: const TextStyle(fontSize: 11),
+        ),
+      ]),
     );
   }
 
--- flutterfirebase/lib/screens/home_page.dart
+++ flutterfirebase/lib/screens/home_page.dart
@@ -18,7 +18,7 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Chat"), actions: [
+      appBar: AppBar(title: const Text("Messages"), actions: [
         IconButton(
             onPressed: () {
               firebaseAuthInstance.signOut();
@@ -47,7 +47,40 @@ class _HomeState extends State<Home> {
     Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
     if (firebaseAuthInstance.currentUser!.email != data["email"]) {
       return ListTile(
-        title: Text(data["email"]),
+        title: Container(
+          margin: const EdgeInsets.only(top: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 10,
+                    spreadRadius: 2,
+                    offset: Offset(0, 2))
+              ]),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                height: 30,
+                child: Row(
+                  children: [
+                    const ClipRRect(
+                      child: Icon(
+                        Icons.person,
+                        size: 40,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Text(data["email"]),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
         onTap: () {
           Navigator.push(
             context,
